ae19aba9cabf8fde1e7a6ac55e54ae2c25db9582
shella12 <ayeshaarshad4567@gmail.com>
2022-07-28 23:05:30 +0500

    Fix linter errors

--- Js/navbar.js
+++ Js/navbar.js
@@ -1,15 +1,13 @@
-const nav=document.querySelector('nav');
-const navMob=document.querySelector('.mob-nav');
-const navLink=document.querySelector('.nav-links');
-const navBar=document.querySelector('.navbar');
-const main=document.querySelector('.main');
+const nav = document.querySelector('nav');
+const navMob = document.querySelector('.mob-nav');
+const navLink = document.querySelector('.nav-links');
+const navBar = document.querySelector('.navbar');
+const main = document.querySelector('.main');
 
-
-navMob.addEventListener('click',function(){
-nav.classList.toggle('active');
-navMob.classList.toggle('active');
-navLink.classList.toggle('active');
-navBar.classList.toggle('active');
-main.classList.toggle('active');
-
+navMob.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  navMob.classList.toggle('active');
+  navLink.classList.toggle('active');
+  navBar.classList.toggle('active');
+  main.classList.toggle('active');
 });
