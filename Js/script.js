5c86b2cf92642996e4dea0b9cc8919a12a7dc2b5
shella12 <ayeshaarshad4567@gmail.com>
2022-08-07 16:37:33 +0500

    Modify website

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,17 @@
 const arr = [
+  
+  {
+    image: './images/speaker_01.png',
+    title: 'Yochai Benkler',
+    occupation: 'Professor at Harvard Law School',
+    detail: 'By developing Asia\'s first Internet protocol network SDN and leading Korea\'s first private line Internet connection in 1990,it ushered in the era of the Internet in earnest.',
+  },
+  {
+    image: './images/speaker_02.png',
+    title: 'Jeon Gil-nam Kilnam Chon',
+    occupation: 'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)',
+    detail: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.(Main books: The Wealth of the Network , Penguins and Leviathan)',
+  },
   {
     image: './images/speaker_03.png',
     title: 'Carol Sue. Dwecker, Ph.D.',
@@ -20,7 +33,7 @@ const arr = [
     title: 'Ryan Merkley Ryan Merkley',
     occupation: 'Creative Commons CEO, Former Mozilla Foundation COO',
     detail: 'He led open source projects at the Mozilla Foundation and joined CC as CEO in 2014. He has been active in open movements such as open government and open source',
-  },
+  }
 ];
 
 // create model
@@ -42,86 +55,13 @@ const cardsDiv = document.createElement('div');
 cardsDiv.className = 'cards';
 speaker.appendChild(cardsDiv);
 
-// card
-
-const card = document.createElement('div');
-card.className = 'card';
-cardsDiv.appendChild(card);
-
-const cardImage = document.createElement('img');
-cardImage.src = './images/speaker_01.png';
-card.appendChild(cardImage);
-
-const cardContent = document.createElement('div');
-cardContent.className = 'card-content';
-card.appendChild(cardContent);
-
-const cardTitle = document.createElement('h3');
-cardTitle.className = 'card-title';
-cardTitle.innerHTML = 'Yochai Benkler';
-cardContent.appendChild(cardTitle);
-
-const cardOccupation = document.createElement('p');
-cardOccupation.className = 'card-occupation';
-cardOccupation.innerHTML = 'Professor at Harvard Law School';
-cardContent.appendChild(cardOccupation);
-
-const cardLine = document.createElement('hr');
-cardLine.className = 'card-line';
-cardContent.appendChild(cardLine);
-
-const cardDetail = document.createElement('p');
-cardDetail.className = 'card-detail';
-cardDetail.innerHTML = "By developing Asia's first Internet protocol network SDN and leading Korea's first private line Internet connection in 1990,it ushered in the era of the Internet in earnest.";
-cardContent.appendChild(cardDetail);
-
-// card2
-
-const card2 = document.createElement('div');
-card2.className = 'card';
-cardsDiv.appendChild(card2);
-
-const cardImage2 = document.createElement('img');
-cardImage2.src = './images/speaker_02.png';
-card2.appendChild(cardImage2);
-
-const cardContent2 = document.createElement('div');
-cardContent2.className = 'card-content';
-card2.appendChild(cardContent2);
-
-const cardTitle2 = document.createElement('h3');
-cardTitle2.className = 'card-title';
-cardTitle2.innerHTML = 'Jeon Gil-nam Kilnam Chon';
-cardContent2.appendChild(cardTitle2);
-
-const cardOccupation2 = document.createElement('p');
-cardOccupation2.className = 'card-occupation';
-cardOccupation2.innerHTML = 'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)';
-cardContent2.appendChild(cardOccupation2);
-
-const cardLine2 = document.createElement('hr');
-cardLine2.className = 'card-line';
-cardContent2.appendChild(cardLine2);
-
-const cardDetail2 = document.createElement('p');
-cardDetail2.className = 'card-detail';
-cardDetail2.innerHTML = 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.(Main books: The Wealth of the Network , Penguins and Leviathan)';
-cardContent2.appendChild(cardDetail2);
-
-const speakerBtn = document.createElement('button');
-speakerBtn.className = 'speaker-btn';
-speakerBtn.innerText = 'More';
-speaker.appendChild(speakerBtn);
-cardsDiv.className = 'cards';
-
-// card loop
-
-speakerBtn.addEventListener('click', () => {
-  const cardsDiv2 = document.createElement('div');
-  cardsDiv2.className = 'cards';
-  speaker.appendChild(cardsDiv2);
-
+//card function
+function createSpeaker(cardsDiv2, start=null){
   arr.forEach((element, index) => {
+    if(start!==null && index < start){
+      index+=1;
+      return;
+    }
     const card = document.createElement('div');
     card.className = 'card';
     cardsDiv2.appendChild(card);
@@ -153,6 +93,26 @@ speakerBtn.addEventListener('click', () => {
     cardDetail.innerHTML = arr[index].detail;
     cardContent.appendChild(cardDetail);
   });
+
+}
+
+createSpeaker(cardsDiv);
+
+window.addEventListener('resize', ()=>{
+
+});
+
+const speakerBtn = document.createElement('button');
+speakerBtn.className = 'speaker-btn';
+speakerBtn.innerText = 'More';
+speaker.appendChild(speakerBtn);
+cardsDiv.className = 'cards';
+
+speakerBtn.addEventListener('click', () => {
+  const cardsDiv2 = document.createElement('div');
+  cardsDiv2.className = 'cards cards-mob';
+  speaker.appendChild(cardsDiv2);
+  createSpeaker(cardsDiv2, 2);
 
   speakerBtn.classList.add('active');
 
